7240ac08c1562f7bc6dadbb895f85aec422e1ebe
Guillaume Collin <collin.guigui@gmail.com>
2017-03-10 09:36:32 +0100

    Ajout des JSON

--- myApp/server/main.js
+++ myApp/server/main.js
@@ -1,6 +1,19 @@
 
 Meteor.startup(() => {
   // code to run on server at startup
+if (Auteurs.find().count() === 0) {
+	Auteurs.insert(auteursJson)
+  }
 
+if (Piles.find().count() === 0) {
+	Piles.insert(pilesJson)
+  }
+
+if (Planete.find().count() === 0) {
+	Planete.insert(planeteJson)
+  }
   
+if (Galeries.find().count() === 0) {
+  }
+
 });
